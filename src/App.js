52e55e4ba6e875ba3ff14e306e780f3e572f56c6
kiki7000 <devkiki7000@gmail.com>
2021-03-06 18:02:16 +0900

    refactor: use state for changing time

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-    setInterval(() => {
-        let today = new Date();
-        let hour = `0${today.getHours()}`.slice(-2);
-        let minutes = `0${today.getMinutes()}`.slice(-2);
-        let second = `0${today.getSeconds()}`.slice(-2);
+class App extends Component {
+    constructor() {
+        super();
 
-        document.getElementById('clock').innerText =  `${hour} : ${minutes} : ${second}`;
-        document.getElementById('date').innerText = new Date().toLocaleDateString();
-    }, 1000);
+        this.state = {
+            hour: null,
+            minutes: null,
+            second: null,
+            dateString: null
+        };
+    }
 
-    return (
-        <div className="App">
-            <h1 className="Title">Web Time</h1>
-            <hr/>
+    componentDidMount() {
+        setInterval(() => {
+            let today = new Date();
+            this.setState({
+                hour: `0${today.getHours()}`.slice(-2),
+                minutes: `0${today.getMinutes()}`.slice(-2),
+                second: `0${today.getSeconds()}`.slice(-2),
+                dateString: today.toLocaleDateString()
+            });
+        }, 1000);
+    }
 
-            <p className="date" id="date" />
-            <p className="clock" id="clock" />
+    render() {
+        return (
+            <div className="App">
+                <h1 className="Title">Web Time</h1>
+                <hr/>
 
-            <a href="https://github.com/ERRrOR404/WebClock"><i className="fab fa-github"></i></a>
-            <p className="footer">Copyright {new Date().getFullYear()} ERRrOR, All rights reserved.</p>
-        </div>
-  );
+                <p className="date" id="date">{this.state.dateString || ''}</p>
+                <p className="clock" id="clock">{this.state.hour ? `${this.state.hour} : ${this.state.minutes} : ${this.state.second}` : 'Calculating'}</p>
+
+                <a href="https://github.com/ERRrOR404/WebClock"><i className="fab fa-github"></i></a>
+                <p className="footer">Copyright {new Date().getFullYear()} ERRrOR, All rights reserved.</p>
+            </div>
+        )
+    }
 }
 
 export default App;

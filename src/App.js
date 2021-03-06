b236838e3d06151573761168b0bc8004974b9a8e
kiki7000 <devkiki7000@gmail.com>
2021-03-06 17:54:24 +0900

    refactor: Use slice for displaying time & built-in function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-
-    setInterval(function() {
+    setInterval(() => {
         let today = new Date();
-        let year = today.getFullYear(); // 년도
-        let month = today.getMonth() + 1;
-        let date = today.getDate();  // 날짜
-        let hour = today.getHours();
-        let minutes = today.getMinutes();
-        let second = today.getSeconds();
+        let hour = `0${today.getHours()}`.slice(-2);
+        let minutes = `0${today.getMinutes()}`.slice(-2);
+        let second = `0${today.getSeconds()}`.slice(-2);
 
-        if(month.toString().length === 1)
-            month = "0" + month.toString();
-        if(date.toString().length === 1)
-            date = "0" + date.toString();
-        if(hour.toString().length === 1)
-            hour = "0" + hour.toString();
-        if(minutes.toString().length === 1)
-            minutes = "0" + minutes.toString();
-        if(second.toString().length === 1)
-            second = "0" + second.toString();
+        document.getElementById('clock').innerText =  `${hour} : ${minutes} : ${second}`;
+        document.getElementById('date').innerText = new Date().toLocaleDateString();
+    }, 1000);
 
-        document.getElementById('clock').innerText = hour + " : " + minutes + " : " + second;
-        document.getElementById('date').innerText = year + "." + month + "." + date;
-    }, 100);
-
-  return (
-    <div className="App">
-      <body className="App-header">
-        <div>
+    return (
+        <div className="App">
             <h1 className="Title">Web Time</h1>
             <hr/>
-        </div>
-        <div style={{width: 1000, height: 500}}>
+
             <p className="date" id="date" />
             <p className="clock" id="clock" />
+
+            <a href="https://github.com/ERRrOR404/WebClock"><i className="fab fa-github"></i></a>
+            <p className="footer">Copyright {new Date().getFullYear()} ERRrOR, All rights reserved.</p>
         </div>
-        <a href="https://github.com/ERRrOR404/WebClock"><i className="fab fa-github"></i></a>
-        <p className="footer">Copyright 2021 ERRrOR, All rights reserved.</p>
-      </body>
-    </div>
   );
 }
 
